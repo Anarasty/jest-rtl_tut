bf2b0eb6d9341bf71473cf0a012303b4f4933a61
Anarasty <murloc.ninja16@gmail.com>
2026-07-19 16:29:54 +0300

    test increment

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -1,11 +1,21 @@
 import { describe, expect, it } from "vitest";
 import Counter from "./Counter";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 describe(Counter, () => {
   it("counter displays correct initial count", () => {
     const { getByTestId } = render(<Counter initialCount={0} />);
     const countValue = Number(getByTestId("count").textContent);
     expect(countValue).toEqual(0);
+  });
+
+  it("count should increment by 1 if the increment button is clicked", () => {
+    const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
+    const incrementBttn = getByRole("button", { name: "Increment" });
+    const countValue1 = Number(getByTestId("count").textContent);
+    expect(countValue1).toEqual(0);
+    fireEvent.click(incrementBttn);
+    const countValue2 = Number(getByTestId("count").textContent);
+    expect(countValue2).toEqual(1);
   });
 });
